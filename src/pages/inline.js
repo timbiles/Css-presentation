c97fb734797d42be43059071d9efd211e224fe5c
Tim Biles <timbilestimbiles@gmail.com>
2018-11-23 23:25:58 -0600

    changed to functional component

--- src/pages/inline.js
+++ src/pages/inline.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 
 const text = {
-        color: 'blue'
-}
+  color: 'blue'
+};
 
-class inline extends Component {
-    render() {
-        return (
-            <>
-                <h1 style={{color: 'green', fontSize: '4em'}}>Inline Styles</h1>
-                <p style={text}>Plz don't do it</p>
-            </>
-        );
-    }
-}
+const inline = () => {
+  return (
+    <>
+      <h1 style={{ color: 'green', fontSize: '4em' }}>Inline Styles</h1>
+      <p style={text}>Plz don't do it</p>
+    </>
+  );
+};
 
 export default inline;
